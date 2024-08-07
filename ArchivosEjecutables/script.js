52656aa5b462415af408a2621ddb0c2b2c68e56a
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-08-07 13:12:13 -0400

    Comienzo del proyecto!

---
+++ ArchivosEjecutables/script.js
@@ -0,0 +1,9 @@
+function suma(numero)
+{
+    // AQUÍ SE ESCRIBE UNA SENTENCIA DE CÓDIGO CON ALGUNA TAREA Y/O CÁLCULO EN ESPECÍFICO...
+
+    return numero + numero;
+}
+
+suma(7);
+document.getElementById("titulo").innerHTML = suma(7);
