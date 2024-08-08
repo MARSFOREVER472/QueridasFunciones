92d271168bd02b39a298e053c70abee17f535713
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-08-08 10:57:59 -0400

    Suma efectuada con antelación.

--- ArchivosEjecutables/script.js
+++ ArchivosEjecutables/script.js
@@ -1,9 +1,19 @@
-function suma(numero)
+function suma(numero1, numero2)
 {
     // AQUÍ SE ESCRIBE UNA SENTENCIA DE CÓDIGO CON ALGUNA TAREA Y/O CÁLCULO EN ESPECÍFICO...
 
-    return numero + numero;
+    return numero1 + numero2;
 }
 
-suma(7);
-document.getElementById("titulo").innerHTML = suma(7);
+// suma(7);
+
+// VAMOS A DECLARAR 2 TÉRMINOS NUMÉRICOS PARA EFECTUAR LA SUMA...
+
+var x = 7; // NÚMERO 1.
+var y = 3; // NÚMERO 2.
+
+// document.getElementById("titulo").innerHTML = suma(7,3);
+
+// document.getElementById("titulo").innerHTML = suma("Marcelo", "Biaggini"); // SOLAMENTE EN ESTA LÍNEA DE CÓDIGO ES A MODO DE EJEMPLO, NO SE PUEDE UTILIZAR ESTA FUNCIÓN MEDIANTE 2 STRINGS.
+
+document.getElementById("titulo").innerHTML = suma(x,y);
